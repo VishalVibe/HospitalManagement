3ff5db46928153dd78db62f874c8ba48f05b73b2
VishalVibe <136216047+VishalVibe@users.noreply.github.com>
2026-04-01 10:51:24 +0530

    modified patient entity

--- hospitalmanagementsystem/src/main/java/com/cg/hospitalmanagementsystem/entity/Patient.java
+++ hospitalmanagementsystem/src/main/java/com/cg/hospitalmanagementsystem/entity/Patient.java
@@ -14,25 +14,34 @@ import java.util.List;
 @Builder
 public class Patient {
 
+
     @Id
     @Column(name = "SSN")
     private Integer ssn;
 
+
     @Column(name = "Name")
     private String name;
+
 
     @Column(name="Address")
     private String address;
 
+
     @Column(name = "Phone")
     private String phone;
 
+
     @Column(name = "InsuranceID")
     private Integer insuranceId;
+
 
     @ManyToOne
     @JoinColumn(name = "PCP", referencedColumnName = "EmployeeID")
     private Physician primaryCarePhysician;
 
 
+
+
 }
+
